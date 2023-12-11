fc13e22a1b66878fd840c911bcba255edae04926
Sabih Ismail <sabih.ismail@gmail.com>
2023-12-10 19:02:20 -0500

    impl

--- GithubStarMerger/Program.cs
+++ GithubStarMerger/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace GithubStarMerger
+﻿using CommandLine;
+
+namespace GithubStarMerger
 {
-    class Program
+    public static class Program
     {
+        private const string PROJECT_NAME = "GithubStarMerger";
+        
+        // ReSharper disable once ClassNeverInstantiated.Local
+        private class CommandLineOptions
+        {
+            [Option('r', "repository-name", Required = false, Default = PROJECT_NAME, HelpText = "Repository name. Default is '" + PROJECT_NAME + "'")]
+            public string RepositoryName { get; }
+        }
+        
         public static void Main(string[] args)
         {
+            var options = Parser.Default.ParseArguments<CommandLineOptions>(args);
+            var repositoryName = options.Value.RepositoryName;
+            
             
         }
     }
